2457bf12bc059f5c8c07277bbaf851689b083b69
Carlos Roberto <carona_jr@hotmail.com>
2020-05-02 16:27:54 -0300

    mod updates

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -47,8 +47,8 @@ router.post('/user', async (req, res) => {
 router.patch('/user/:cpf', async(req, res) => {
     const cpf = req.params.cpf
     try {
-        const query = await searchByKeyAndUpdate(req.body, 'cpf', cpf, queryFindByCpf, ['cep', 'num_casa'], ['nome', 'rua', 'cep'])
-        res.send()
+        const query = await searchByKeyAndUpdate(req.body, 'cpf', cpf, queryFindByCpf, ['nome', 'rua', 'cep'], ['cep', 'num_casa'])
+        res.send(query)
     } catch (e) {
         res.status(400).send(e)
     }
--- backend/src/services/update.js
+++ backend/src/services/update.js
@@ -1,15 +1,16 @@
 /* 
-    A função searchByKeyAndUpdate serve para alterar algum dado em qualquer tabela, informando a chave e outros dados
+    A função searchByKeyAndUpdate procura alguma coluna e altera seu valor de acordo com a chave passada por parâmetro.
+    Se o parâmetro de busca for uma data, colocar o campo da data no vetor de inteiros.
 
-    *** USO ***
+    *** EXEMPLO ***
 
     searchByKeyAndUpdate(
-        CORPO DA REQUISIÇÃO COM OS DADOS PARA ALTERAR (req.body), 
-        CAMPO A SER PROCURADO (CHAVE, ex. 'cpf'),
-        VALOR DA CHAVE A SER PROCURADO (ex '123456') 
-        OBJETO COM O TEXT (MODELS, ex queryFindByCpf), 
-        VETOR DOS VALORES DO TIPO INTEGER (['num_cara', 'cep']), 
-        VETOR DAS ALTERAÕES PERMITIDAS (['nome', 'rua', 'cep'])
+        Requisição com os dados para alterar (req.body), 
+        Coluna a ser utilizada na pesquisa (CHAVE, ex. 'cpf'),
+        Valor exato da chave (ex '123456') 
+        Objeto do modelo a ser utilizado como pesquisa (MODELS, ex queryFindByCpf), 
+        Vetor das alterações permitidas (['nome', 'rua', 'cep']),
+        Vetor das colunas inteiras da tabela, opcional se não houver (['num_cara', 'cep'])
     )
 
     A função retorna uma string no padrão correto para ser inserida na consulta
@@ -18,55 +19,76 @@
 
 const pool = require('../db/elephant-sql')
 
-// Verfica se o atributo é inteiro com o vetor de inteiros passados por parametro
-const verifyInteger = (arr, attribute) => {
-    return arr.every(value => attribute !== value)
-}
-
 // Verifica se o update é possível
 const verifyAllowed = (arr, allowedUpdates) => {
     return arr.every(update => allowedUpdates.includes(update))
 }
 
-const searchByKeyAndUpdate = (data, key, keyword, objValues, arrInt = [], allowedUpdates = []) => {
+const searchByKeyAndUpdate = (data, key, keyword, objValues, allowedUpdates = [], arrInt = []) => {
     return new Promise(async (resolve, reject) => {
 
         // Verifica se o update é permitido
         const dataKeys = Object.keys(data)
         if (!verifyAllowed(dataKeys, allowedUpdates))
-            reject({ erro: 'Não foi possível realizar o update, uma das alterações não é permitida' })
+            reject({
+                erro: 'Não foi possível realizar o update, uma das alterações não é permitida',
+                valores_enviados: data,
+                valores_permitidos: allowedUpdates,
+                valores_inteiros: arrInt,
+                chave: key,
+                valor: keyword,
+                obj: objValues
+            })
 
         // Realiza a busca no banco de dados, pela key fornecida, do usuário passado por parametro
-        objValues.values = [keyword]
         try {
+            objValues.values = [keyword]
             const user = await pool.query(objValues)
 
             // Cria um objeto com os valores diferentes daqueles armazenados no banco, verificando, assim, se o dado foi alterado ou não
-            let update = {}
+            let objForUpdates = {}
             const userKeys = Object.keys(user.rows[0])
-            userKeys.map(value => data[value] !== user[value] ? update[value] = data[value] : false)
+            userKeys.map(value => data[value] !== user[value] ? objForUpdates[value] = data[value] : false)
 
             // Concatena o comando UPDATE com os valores alterados na requisição, o restante é ignorado
-            const updateKeys = Object.keys(update)
-            let str = 'UPDATE Usuario SET '
+            const updateKeys = Object.keys(objForUpdates)
+            let createNewQuery = 'UPDATE Usuario SET '
             updateKeys.map(value => {
-                let newStr
-                if (verifyInteger(arrInt, value))
-                    newStr = `${value} = '${update[value]}', `
+                let setColumnsToUpdate
+                if (!arrInt.includes(value))
+                    setColumnsToUpdate = `${value} = '${objForUpdates[value]}', `
                 else {
-                    newStr = `${value} = ${update[value]}, `
+                    setColumnsToUpdate = `${value} = ${objForUpdates[value]}, `
                 }
-                str += newStr
+                createNewQuery += setColumnsToUpdate
             })
 
             // Concatena o WHERE a fim de alterar somente pela chave fornecida especificado
-            let updateStr = str.split('').slice(0, str.length - 2).join('')
-            updateStr += ` WHERE ${key} = '${keyword}'`
+            let queryToUpdate = createNewQuery.split('').slice(0, createNewQuery.length - 2).join('')
 
-            await pool.query(updateStr)
-            resolve({ sucesso: true })
+            if(!arrInt.includes(key))
+                queryToUpdate += ` WHERE ${key} = '${keyword}'`
+            else {
+                queryToUpdate += ` WHERE ${key} = ${keyword}`
+            }
+
+            // Realiza o update no banco de dados
+            const updateUser =  await pool.query(queryToUpdate)
+            resolve({ 
+                sucesso: true,
+                command: updateUser.command,
+                rows: updateUser.rowCount
+            })
         } catch (e) {
-            reject({ erro: `impossível atualizar o valor ${keyword} do/a ${key} na tabela`})
+            reject({
+                erro: `Não foi possível realizar o update, uma das alterações não foi realizada pelo banco`,
+                valores_enviados: data,
+                valores_permitidos: allowedUpdates,
+                valores_inteiros: arrInt,
+                chave: key,
+                valor: keyword,
+                obj: objValues
+            })
         }
     })
 }
