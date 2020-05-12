61ec4eb665424375599e2f1de55bef38184a1b6e
Carlos Roberto <carona_jr@hotmail.com>
2020-05-12 18:51:33 -0300

    mod header & man

--- academia/src/components/Header.js
+++ academia/src/components/Header.js
@@ -1,18 +1,37 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import FormControl from 'react-bootstrap/FormControl'
 
 import './Header.css'
 
 export default function Header() {
     return (
-        <div className="container-header">
-            <div>
-                <h1 className="display-3">Academia Pokémon</h1>
-            </div>
-            <div>
-                <Button className="px-md-3 mx-md-3" variant="light">Cadastrar</Button>
-                <Button className="px-md-3 mx-md-3" variant="light">Entrar</Button>
-            </div>
-        </div>
+        <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <Navbar.Brand href="#home">
+                <h2 className="logo display-5">Academia Pokémon</h2>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar >
     )
 }
--- academia/src/pages/Main.js
+++ academia/src/pages/Main.js
@@ -7,15 +7,15 @@ import './Main.css'
 
 export default function Main() {
     return (
-        <div className="container-main">
-            <div className="container-login">
-                <div className="p-lg-5">
+        <div className="container-main d-flex justify-content-center align-items-center">
+            <div className="d-flex justify-content-center align-items-center">
+                <div className=" container-login p-5 rounded shadow">
                     <h2 className="display-5 text-center">Entrar</h2>
                     <div>
                         <Form>
                             <Form.Group controlId="formBasicEmail">
                                 <Form.Label>CPF</Form.Label>
-                                <Form.Control className="p-lg-4" type="text" placeholder="123456789" required pattern="[0-9]{11}"/>
+                                <Form.Control className="p-4" type="text" placeholder="123456789" required pattern="[0-9]{11}"/>
                                 <Form.Text className="text-muted">
                                     Seu CPF não será compartilhado com ninguém.
                                 </Form.Text>
@@ -23,14 +23,14 @@ export default function Main() {
 
                             <Form.Group controlId="formBasicPassword">
                                 <Form.Label>Senha</Form.Label>
-                                <Form.Control className="p-lg-4" type="password" placeholder="**********" required/>
+                                <Form.Control className="p-4" type="password" placeholder="**********" required/>
                             </Form.Group>
-                            <Button className="my-lg-4" variant="primary" type="submit" size="lg" block>
+                            <Button className="my-4" variant="primary" type="submit" size="lg" block>
                                 Entrar
                             </Button>
                         </Form>
-                            <p className="display-5 my-lg-0">Se ainda não possui uma conta:</p>
-                            <Button className="my-lg-1 text-center" variant="outline-light" type="submit" size="lg" block>
+                            <p className="display-5 my-0">Se ainda não possui uma conta:</p>
+                            <Button className="my-1 text-center" variant="outline-light" type="submit" size="lg" block>
                                 Cadastre-se 
                             </Button>
                     </div>
