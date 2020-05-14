bdb7810602b2d49e3e857ab23c0d046a3f66c305
Carlos Roberto Nascimento Junior <carona_jr@hotmail.com>
2020-05-13 21:16:05 -0300

    conflicts

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -2,16 +2,10 @@ import React from 'react'
 
 import LoginNav from '../components/LoginHeader'
 
-<<<<<<< HEAD
-=======
-import img from '../assets/images/bd.png'
->>>>>>> 3171f6538b6f4e5acddd125a81a292bdc32bac3c
-
 import './About.css'
 
 export default function Main() {
     return (
-<<<<<<< HEAD
         <div>
             {
                     <div className="container-maintxt d-flex justify-content-center align-items-center">
@@ -28,22 +22,6 @@ export default function Main() {
                             </div>   
                     </div>
             }
-=======
-        <div className="container-about" >
-            <LoginNav />
-            <div className="mt-5 pt-5 d-flex justify-content-center align-items-center flex-column">
-                <div className="mb-5">
-                    <h1>Sobre nós</h1>
-                </div>
-                <div className="p-5 bg-light">
-                    <h2 className="text-center display-5 mb-5">Trabalho de Banco de Dados</h2>
-                    <p>O projeto é um trabalho da disciplina de banco de dados I a fim de explorar os conceitos aprendidos em sala de aula com a linguagem SQL. </p>
-                    <p>Este trabalho tem como objetivo o desenvolvimento do banco de dados e uma interface gráfica para visualização dos dados contidos neste banco.</p>
-                    <h5 className="display-5 mb-5 mt-4" >Diagrama modelo entidade</h5>
-                    <img className="w-100 h-auto" src={img} />
-                </div>
-            </div>
->>>>>>> 3171f6538b6f4e5acddd125a81a292bdc32bac3c
         </div>
     )
 }
