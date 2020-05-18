5bde91dc407e7bcbe4ade78bea4351286764f53a
Carlos Roberto <carona_jr@hotmail.com>
2020-05-18 18:29:45 -0300

    mod user home

--- frontend/src/pages/Home/User.js
+++ frontend/src/pages/Home/User.js
@@ -23,18 +23,17 @@ export default function User({ history }) {
                                 <div className="container-data">
                                     <div className="container-data-pokemon">
                                         <h5>Seus pokémons recentes</h5>
-                                        <PokemonList route="/pokemon/top"/>
+                                        <PokemonList route="/pokemon/top" />
                                     </div>
                                     <div className="p-0 m-0 container-data-chart">
                                         <h5>Seus pokémons por classificação</h5>
                                         <PokemonByTypeChart />
                                     </div>
                                 </div>
-                                <div className="container-data-calendar mt-3">
+                                <div className="container-data-calendar">
                                     <h5>Veja o seu histórico de pokémons</h5>
                                     <PokemonCalendar />
                                 </div>
-
                             </div>
                             <SideNav divMain={divMain} />
                         </div>
