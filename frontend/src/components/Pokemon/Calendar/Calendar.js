4cf947c54024401c1876bb1c223a038ce33350a7
Carlos Roberto <carona_jr@hotmail.com>
2020-05-19 21:38:14 -0300

    add calendar

--- frontend/src/components/Pokemon/Calendar/Calendar.js
+++ frontend/src/components/Pokemon/Calendar/Calendar.js
@@ -1,38 +1,15 @@
 import React from 'react'
 import ReactTooltip from 'react-tooltip'
 import CalendarHeatmap from 'react-calendar-heatmap'
-import 'react-calendar-heatmap/dist/styles.css'
 import './styles.css'
 
-const today = new Date()
-
-function shiftDate(date, numDays) {
-    const newDate = new Date(date);
-    newDate.setDate(newDate.getDate() + numDays);
-    return newDate;
-}
-
-function getRange(count) {
-    return Array.from({ length: count }, (_, i) => i);
-}
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-export default function Calendar({ data }) {
-    const randomValues = getRange(200).map(index => {
-        return {
-            date: shiftDate(today, -index),
-            count: getRandomInt(1, 3)
-        }
-    })
+export default function Calendar({ data, today, past }) {
     return (
         <div>
             <CalendarHeatmap
-                startDate={shiftDate(today, -150)}
+                startDate={past}
                 endDate={today}
-                values={randomValues}
+                values={data}
                 classForValue={value => {
                     if (!value) {
                         return 'color-empty';
@@ -40,8 +17,12 @@ export default function Calendar({ data }) {
                     return `color-github-${value.count}`
                 }}
                 tooltipDataAttrs={value => {
+                    if (value.date === null)
+                        return {
+                            'data-tip': 'Nada foi adicionado'
+                        }
                     return {
-                        'data-tip': `Em ${value.date.toISOString().slice(0, 10)} foi adicionado ${value.count} pokémon(s)`
+                        'data-tip': `Em ${value.date} foi adicionado ${value.count} pokémon(s)`
                     }
                 }}
                 onClick={value => alert(`Você adicionou ${value.count} pokémon(s)`)}
--- frontend/src/components/Pokemon/Calendar/PokemonCalendar.js
+++ frontend/src/components/Pokemon/Calendar/PokemonCalendar.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Calendar from './Calendar'
-// import Spinner from 'react-loading'
-// import { api } from '../../../services/api'
+import Spinner from 'react-loading'
+import { api } from '../../../services/api'
+
+const today = new Date()
 
 export default function PokemonCalendar() {
+    const [data, setData] = useState()
+    const [formattedDate, setFormattedDate] = useState()
+    const [responseData, setResponseData] = useState()
+    const formattedToday = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
+
+    async function loadDays() {
+        const unix_timestamp = (today.getTime() / 1000).toFixed(0) - 86400 * 150 // unix - 150 dias para trás, 1 dia = 86400 segundos
+        const pastDate = new Date(unix_timestamp * 1000)
+        const formatted = `${pastDate.getFullYear()}-${pastDate.getMonth() + 1}-${pastDate.getDate()}`
+        setFormattedDate(formatted)
+
+        try {
+            const cpf = localStorage.getItem('cpf')
+            const response = await api.get('/pokemon/date',{
+                headers: {
+                    Authorization: 'Bearer ' + cpf,
+                    DateToSearch: '2020-05-07'
+                }
+            })
+            setData(response.data)
+        } catch (e) {
+            setResponseData('empty')
+        }
+    }
+
+
+    useEffect(() => {
+        loadDays()
+    }, [])
+
     return (
-        <Calendar />
+        (data && data.length > 0) ? (
+            <>
+                <Calendar data={data} today={formattedToday} past={formattedDate}/>
+            </>
+        ) : (responseData === 'empty') ? (
+            <div>
+                <p>Vc não adicionou nenhum pokémon :(</p>
+            </div>
+        ) : (
+            <div className="d-flex justify-content-center my-5 py-5" >
+                <Spinner type="bars" width={'32px'} height={'32px'} color={'blue'}/>
+            </div>
+        )
     )
 }
