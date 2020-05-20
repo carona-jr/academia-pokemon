90779fda37bed9fae6655ddb40db3f291a142df4
Carlos Roberto <carona_jr@hotmail.com>
2020-05-20 14:14:30 -0300

    rm comments

--- backend/src/routes/pokemon.js
+++ backend/src/routes/pokemon.js
@@ -98,38 +98,6 @@ router.get('/pokemon/date', auth, async (req, res) => {
     }
 })
 
-// GET /tasks?completed=true
-// GET /tasks?limit=2&skip=20
-// GET /tasks?sortBy=createdAt:desc
-// router.get('/tasks', auth, async(req, res) => {
-//     const match = {}
-//     const sort = {}
-
-//     if (req.query.completed) {
-//         match.completed = req.query.completed === 'true'
-//     }
-
-//     if (req.query.sortBy) {
-//         const parts = req.query.sortBy.split(':')
-//         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
-//     }
-
-//     try {
-//         await req.user.populate({
-//             path: 'tasks',
-//             match,
-//             options: {
-//                 limit: parseInt(req.query.limit),
-//                 skip: parseInt(req.query.skip),
-//                 sort
-//             }
-//         }).execPopulate()
-//         res.send(req.user.tasks)
-//     } catch (e) {
-//         res.status(500).send()
-//     }
-// })
-
 router.get('/pokemon/all', auth, async  (req, res) => {
     try {
         if (req.query.sortBy && req.query.limit) {
