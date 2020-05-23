54a0cffa7877ee00714f90b38e6b31d6c6289591
Carlos Roberto <carona_jr@hotmail.com>
2020-05-22 22:44:09 -0300

    mod 404

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
+import Button from 'react-bootstrap/Button'
 import './NotFound.css'
+import charizard from '../assets/images/charizard.gif'
 
-export default function NotFound() {
+export default function NotFound({ history }) {
     return (
-        <div>
-            <div id="notfound">
-                <div class="notfound">
-                    <div class="notfound-404">
-                        <h1>Oops!</h1>
-                        <h2>404 - The Page can't be found</h2>
-                    </div>
-                    <a href="/user">Clique aqui para voltar</a>
+        <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh', backgroundColor: '#f1f1f1' }}>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+                <h2 className="my-4 title-404" style={{ color: '#808080', textTransform: 'uppercase' }}>Page Not Found - 404</h2>
+                <div className="my-4">
+                    <img src={charizard} alt="charizard"/>
+                </div>
+                <div className="my-4">
+                    <Button className="py-2 px-5" variant="outline-danger" type="button" onClick={() => history.push('/user')}>Voltar para a página anterior</Button>
                 </div>
             </div>
         </div>
