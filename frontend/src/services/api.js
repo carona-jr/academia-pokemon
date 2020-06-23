5abfbe917d4032ee27eaecc2254629aefeab4f28
Carlos Roberto Nascimento Junior <carona_jr@hotmail.com>
2020-06-23 20:17:14 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://carona-backend-academia.herokuapp.com'
-    // baseURL: 'http://localhost:3333'
+    // baseURL: 'https://carona-backend-academia.herokuapp.com'
+    baseURL: 'http://localhost:3333'
 })
 
 const apiPokemon = axios.create({
